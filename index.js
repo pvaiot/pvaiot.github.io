c210354bd648cf730df5eecad9de28692bb6aff1
Kartik Bathla <62009089+ceokartik@users.noreply.github.com>
2020-07-26 20:18:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ let fname = prompt("Enter First Name");
 let lname = prompt("Enter Last Name");
 let age = prompt("Enter Age");
 
-let result = `${fname} ${lname}'s age is ${age}`;
+let result = `${fname} ${lname}'s is ${age} years old chutiya`;
 alert(result);
-let fullname = prompt("What is Your Name");
-let result2 = `${fullname}` chutiya hai;
-alert(result2);
+
