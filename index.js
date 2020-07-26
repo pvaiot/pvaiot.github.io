4a6ab72caecfcebbf2a43c3086ed13ef0fd62a2f
Kartik Bathla <62009089+ceokartik@users.noreply.github.com>
2020-07-26 20:15:58 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let fname = prompt("Enter First Name");
+let lname = prompt("Enter Last Name");
+let age = prompt("Enter Age");
+
+let result = `${fname} ${lname}'s age is ${age}`;
+alert(result);
+let fullname = prompt("What is Your Name");
+let result2 = `${fullname}` chutiya hai;
+alert(result2);
