6230e5c755c19ccad6a8c4f075875ff746c0a717
Kartik Bathla <62009089+ceokartik@users.noreply.github.com>
2020-08-01 07:57:32 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let fname = prompt("Enter First Name");
-let lname = prompt("Enter Last Name");
-let age = prompt("Enter Age");
-
-let result = `${fname} ${lname}'s is ${age} years old chutiya`;
-alert(result);
-
